29c18f0364ea54ab3572996818a9672c1c4d626c
Yodieyogaming <groendyl@gmail.com>
2023-10-24 22:04:04 +0200

    rewrite to functions

--- background.js
+++ background.js
@@ -1,21 +1,22 @@
 const fetchedURLs = {};
 let searchPower = false;
+let dayOfWeek;
 
-chrome.runtime.onStartup.addListener(() => {
+function onStartup() {
     chrome.tabs.create({ url: "https://purmerend.jarvis.bit-academy.nl" }, (tab) => {
-        // Listen for tab updates
+        createdTab = tab;
         chrome.tabs.onUpdated.addListener(function onUpdatedListener(tabId, changeInfo) {
             if (tabId === tab.id && changeInfo.status === "complete") {
-                // Page is fully loaded, close the tab
-                chrome.tabs.remove(tab.id);
-                // Remove the listener to avoid unnecessary checks
-                // chrome.tabs.onUpdated.removeListener(onUpdatedListener);
+                if (searchPower) {
+                    chrome.tabs.remove(tab.id); // Close the tab when searchPower is true
+                }
             }
         });
     });
-});
+}
 
-chrome.webRequest.onCompleted.addListener(event => {
+
+function onCompleted(event) {
     if (event.url.includes('streaks')) {
         if (fetchedURLs[event.url]) {
             return;
@@ -32,10 +33,12 @@ chrome.webRequest.onCompleted.addListener(event => {
             })
             .then(data => {
                 const daysThisWeek = data.daysThisWeek;
-                console.log(daysThisWeek);
+                console.log("Days this week:", daysThisWeek);
+                console.log("dayOfWeek:", dayOfWeek);
 
                 if (daysThisWeek.hasOwnProperty(dayOfWeek)) {
                     searchPower = true;
+                    chrome.tabs.remove(createdTab.id);
                 } else {
                     console.log(`Day of the week ${dayOfWeek} is not connected to daysThisWeek.`);
                 }
@@ -50,14 +53,15 @@ chrome.webRequest.onCompleted.addListener(event => {
             });
     }
     return;
-}, { urls: ["<all_urls>"] });
+}
 
-var today = new Date();
-var dayOfWeek = (today.getDay() - 1 + 7) % 7;
+function initializeDayOfWeek() {
+    var today = new Date();
+    dayOfWeek = (today.getDay() - 1 + 7) % 7;
+    console.log(dayOfWeek);
+}
 
-console.log(dayOfWeek);
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+function onTabsUpdated(tabId, changeInfo, tab) {
     if (changeInfo.status === "complete") {
         const currentUrl = tab.url;
         const allowedUrls = [
@@ -78,11 +82,19 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             chrome.tabs.update(tabId, { url: "https://purmerend.jarvis.bit-academy.nl" });
         }
     }
-});
+}
 
 function isAllowedUrl(url, allowedUrls) {
     return allowedUrls.some(allowedUrl => url.startsWith(allowedUrl));
 }
 
-console.log('%cGitHub: %c(https://github.com/Dylan-Kuiper)', 'font-weight: bold; color: blue;', 'font-weight: normal; color: black;');
-console.log('%cThe path to your %cdestiny%c begins with a steady streak.', 'font-weight: bold; color: red;', 'font-weight: bold; color: deepPink;', 'font-weight: bold; color: red;');
+function logInfo() {
+    console.log('%cGitHub: %c(https://github.com/Dylan-Kuiper)', 'font-weight: bold; color: blue;', 'font-weight: normal; color: black;');
+    console.log('%cThe path to your %cdestiny%c begins with a steady streak.', 'font-weight: bold; color: red;', 'font-weight: bold; color: deepPink;', 'font-weight: bold; color: red;');
+}
+
+chrome.runtime.onStartup.addListener(onStartup);
+chrome.webRequest.onCompleted.addListener(onCompleted, { urls: ["<all_urls>"] });
+initializeDayOfWeek();
+chrome.tabs.onUpdated.addListener(onTabsUpdated);
+logInfo();
