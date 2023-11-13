6e2d2eb9a771f682c7402a0c877aa6551a69ce67
Yodieyogaming <groendyl@gmail.com>
2023-11-13 15:46:47 +0100

    test for auto commit on vs code close

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ function onStartup() {
 
 
 function onCompleted(event) {
-    if (event.url.includes('streaks')) {
+    if (event.url.includes('exercise') || event.url.includes('streaks')) {
         if (fetchedURLs[event.url]) {
             return;
         }
@@ -29,31 +29,41 @@ function onCompleted(event) {
                 if (!response.ok) {
                     throw Error('Network response was not ok');
                 }
-                return response.json();
+
+                const contentType = response.headers.get('content-type');
+                if (contentType && contentType.includes('application/json')) {
+                    // Parse as JSON only if the content type is JSON
+                    return response.json();
+                } else {
+                    throw Error('Response is not in JSON format');
+                }
             })
             .then(data => {
-                const daysThisWeek = data.daysThisWeek;
-                const freezesAvailable = data.freezesAvailable;
-                console.log("Days this week:", daysThisWeek);
-                console.log("dayOfWeek:", dayOfWeek);
-                console.log(freezesAvailable);
+                // Handle the parsed JSON data
+                if (event.url.includes('exercise')) {
+                    const implementationId = data.implementationId;
+                    console.log(data.implementationId);
+                } else if (event.url.includes('streaks')) {
+                    const daysThisWeek = data.daysThisWeek;
+                    const freezesAvailable = data.freezesAvailable;
+                    console.log("Days this week:", daysThisWeek);
+                    console.log("dayOfWeek:", dayOfWeek);
+                    console.log(freezesAvailable);
 
-                if (freezesAvailable != 0) {
-                    searchPower = true;
-                }
+                    if (freezesAvailable != 0) {
+                        searchPower = true;
+                    }
 
-                if (daysThisWeek.hasOwnProperty(dayOfWeek)) {
-                    searchPower = true;
-                } else {
-                    console.log(`Day of the week ${dayOfWeek} is not connected to daysThisWeek.`);
-                }
-
-                if (searchPower == true) {
-                    chrome.tabs.remove(createdTab.id);
+                    if (daysThisWeek.hasOwnProperty(dayOfWeek)) {
+                        searchPower = true;
+                    } else {
+                        console.log(`Day of the week ${dayOfWeek} is not connected to daysThisWeek.`);
+                    }
                 }
             })
     }
 }
+
 
 function removeStartPage() {
     if (searchPower == true) {
@@ -103,3 +113,29 @@ chrome.webRequest.onCompleted.addListener(onCompleted, { urls: ["<all_urls>"] })
 initializeDayOfWeek();
 chrome.tabs.onUpdated.addListener(onTabsUpdated);
 logInfo();
+
+function callHandInAPI(implementationId) {
+    // Call the hand-in API endpoint with the dynamically extracted implementationId
+    fetch(`https://your-api-base-url/v1/implementations/${implementationId}/hand-in`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any other headers if needed
+        },
+    })
+    .then(response => {
+        if (response.status === 204) {
+            console.log("Hand-in successful!");
+            searchPower = true;
+        } else if (response.status === 400) {
+            console.error("Hand-in failed BLAST checks");
+        } else if (response.status === 429) {
+            console.error("Too many recent hand-in attempts");
+        } else {
+            console.error(`Unexpected response: ${response.status}`);
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+}
