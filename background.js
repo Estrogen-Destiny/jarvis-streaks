8aacdd04270136cbd962222321a1666abc88aed4
Dylan Kuiper <165335@student.horizoncollege.nl>
2023-11-15 16:00:16 +0100

    test

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
-const fetchedURLs = [];
+const fetchedURLs = {};
 let searchPower = false;
 let dayOfWeek;
+let createdTab; // Declare createdTab variable
 
 function onStartup() {
     chrome.tabs.create({ url: "https://jarvis.bit-academy.nl/" }, (tab) => {
         createdTab = tab;
-        chrome.tabs.onUpdated.addListener(function onUpdatedListener(tabId, changeInfo) {
-            if (tabId === tab.id && changeInfo.status === "complete") {
-                if (searchPower) {
-                    chrome.tabs.remove(tab.id); // Close the tab when searchPower is true
-                }
-            }
-        });
     });
-    listenForHandinXHR();
+
+    // Use chrome.tabs.onUpdated.addListener outside of chrome.tabs.create callback
+    chrome.tabs.onUpdated.addListener(function onUpdatedListener(tabId, changeInfo) {
+        if (tabId === createdTab.id && changeInfo.status === "complete") {
+            if (searchPower) {
+                chrome.tabs.remove(createdTab.id);
+            }
+        }
+    });
 }
 
 function onCompleted(event) {
@@ -29,89 +31,46 @@ function onCompleted(event) {
                 if (!response.ok) {
                     throw Error('Network response was not ok');
                 }
-                return response.text(); // Change this line to get the response as text
+                console.log(response.json);
+                return response.json();
             })
             .then(data => {
-                console.log("Response text:", data); // Log the response text
+                if (event.url.includes('exercise')) {
+                    const implementationId = data.implementationId;
+                    console.log(data.implementationId);
+                } else if (event.url.includes('streaks')) {
+                    const daysThisWeek = data.daysThisWeek;
+                    const freezesAvailable = data.freezesAvailable;
+                    console.log("Days this week:", daysThisWeek);
+                    console.log("dayOfWeek:", dayOfWeek);
+                    console.log(freezesAvailable);
 
-                // Now try to parse as JSON
-                try {
-                    const jsonData = JSON.parse(data);
-                    // Handle the parsed JSON data
-                    if (event.url.includes('exercise')) {
-                        const implementationId = jsonData.implementationId;
-                        console.log(implementationId);
-                    } else if (event.url.includes('streaks')) {
-                        const daysThisWeek = jsonData.daysThisWeek;
-                        const freezesAvailable = jsonData.freezesAvailable;
-                        console.log("Days this week:", daysThisWeek);
-                        console.log("dayOfWeek:", dayOfWeek);
-                        console.log(freezesAvailable);
-
-                        if (freezesAvailable != 0) {
-                            searchPower = true;
-                        }
-
-                        if (daysThisWeek.hasOwnProperty(dayOfWeek)) {
-                            searchPower = true;
-                        }
+                    if (freezesAvailable !== 0 || daysThisWeek.hasOwnProperty(dayOfWeek)) {
+                        searchPower = true;
+                    } else {
+                        console.log(`Day of the week ${dayOfWeek} is not connected to daysThisWeek.`);
                     }
-                } catch (error) {
-                    console.error("Error parsing JSON:", error);
+                }
+
+                // Check for "minimal hand-in" or "hand-in-file-upload"
+                if (event.url.includes('hand-in') || event.url.includes('hand-in-file-upload')) {
+                    searchPower = true;
+                    console.log(searchPower);
+                    // Do something specific for hand-in URLs
                 }
             })
             .catch(error => {
-                console.error("Error fetching data:", error);
+                console.log(error);
+                console.error("Fetch error:", error);
             });
     }
-    if (event.url.includes('hand-in')) {
-        listenForHandinXHR();
+}
+
+function removeStartPage() {
+    if (searchPower == true) {
+        chrome.tabs.remove(createdTab.id);
     }
 }
-
-function listenForHandinXHR() {
-    const handinEndpoint = "https://your-api-url/handin"; // Replace with your actual "handin" API endpoint
-
-    // Use chrome.webRequest to listen for XHR events on the "handin" endpoint
-    chrome.webRequest.onCompleted.addListener(
-        function (details) {
-            // Check if the response code is 204
-            if (details.url === handinEndpoint && details.statusCode === 204) {
-                searchPower = true;
-            }
-        },
-        { urls: [handinEndpoint], types: ["xmlhttprequest"] }
-    );
-}
-
-if (!searchPower) {
-    function checkTimeAndSetSearchPower() {
-        const currentTime = new Date();
-        const pauses = [
-            { start: { hours: 11, minutes: 0 }, end: { hours: 11, minutes: 15 } },
-            { start: { hours: 12, minutes: 30 }, end: { hours: 13, minutes: 15 } },
-            { start: { hours: 14, minutes: 15 }, end: { hours: 14, minutes: 30 } }
-        ];
-
-        // Check if the current time is within one of the specified pauses
-        const isInPause = pauses.some(pause => {
-            const startPauseTime = new Date(currentTime);
-            startPauseTime.setHours(pause.start.hours, pause.start.minutes, 0);
-
-            const endPauseTime = new Date(currentTime);
-            endPauseTime.setHours(pause.end.hours, pause.end.minutes, 0);
-
-            return currentTime >= startPauseTime && currentTime <= endPauseTime;
-        });
-
-        if (isInPause) {
-            searchPower = true;
-        } else {
-            searchPower = false;
-        }
-    }
-}
-
 
 function initializeDayOfWeek() {
     var today = new Date();
@@ -155,4 +114,3 @@ chrome.webRequest.onCompleted.addListener(onCompleted, { urls: ["<all_urls>"] })
 initializeDayOfWeek();
 chrome.tabs.onUpdated.addListener(onTabsUpdated);
 logInfo();
-//make a xhr listener that listens to url handin api endoint and if 204 then searchpower is true
